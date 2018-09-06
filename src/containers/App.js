f76f1d1fec41775accba138a0d0f1d13af66cd76
Stevan Rakic <stevan.rakic@appmodule.net>
2018-09-06 14:40:40 +0200

    JWT Added.

---
+++ src/containers/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Router, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { history } from '../_helpers'
+
+class App extends React.Component {
+
+    
+
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 // Set up your application entry point here...
+
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore'
+
+render (
+    <Provider store={configureStore()}>
+
+    </Provider>,
+    document.getElementById('root')
+);
