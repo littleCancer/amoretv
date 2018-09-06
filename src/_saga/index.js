3a52f4390be9e3adec197d5ddee71cb2ea21673e
Stevan Rakic <stevan.rakic@appmodule.net>
2018-09-06 11:56:01 +0200

    Saga setup

---
+++ src/_saga/index.js
@@ -0,0 +1,23 @@
+import {spawn, call, delay} from 'redux-saga';
+
+const makeRestartable = (saga) => {
+    return function*() {
+        while (true) {
+            try {
+                 yield call(saga);
+                console.error("unexpected root saga termination. The root sagas are supposed to be sagas that live during the whole app lifetime!",saga);
+            } catch (e) {
+                console.error("Saga error, the saga will be restarted",e);
+            }
+            yield delay(1000); // Workaround to avoid infinite error loops
+        }
+    }
+}
+
+const rootSagas = [
+
+].map(makeRestartable);
+
+export default function* rootSaga() {
+    yield rootSagas.map(saga => call(saga));
+}
